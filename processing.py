887419900fd3b75fe566be8d5c5b62d8646a3112
raigor <raigor@mail.ru>
2020-11-17 23:42:21 +0700

    added median and saving at file

--- processing.py
+++ processing.py
@@ -8,6 +8,11 @@ import logging
 import re
 import math
 
+config = {
+    'pos_head': 2000,
+    'pos_tail': 10000
+}
+
 
 class Par_Data():
     def __init__(self, file_name):
@@ -20,13 +25,29 @@ class Par_Data():
             self.par['DeviceName'] = self.dev_name
             self.time_str = f.read(26).decode('ascii')
             self.par['TimeString'] = self.time_str
-            self.chans_max = int.from_bytes(f.read(2), byteorder='little', signed=False)
+            self.chans_max = int.from_bytes(
+                f.read(2), 
+                byteorder='little', 
+                signed=False
+            )
             self.par['ChannelsMax'] = self.chans_max
-            self.real_chan = int.from_bytes(f.read(2), byteorder='little', signed=False)
+            self.real_chan = int.from_bytes(
+                f.read(2), 
+                byteorder='little', 
+                signed=False
+            )
             self.par['RealChannelsQuantity'] = self.real_chan
-            self.real_kadr = int.from_bytes(f.read(8 if self.code_str[16:17] == 'A' else 4), byteorder='little', signed=False)
+            self.real_kadr = int.from_bytes(
+                f.read(8 if self.code_str[16:17] == 'A' else 4), 
+                byteorder='little', 
+                signed=False
+            )
             self.par['RealKadrsQuantity'] = self.real_kadr
-            self.real_samp = int.from_bytes(f.read(8 if self.code_str[16:17] == 'A' else 4), byteorder='little', signed=False)
+            self.real_samp = int.from_bytes(
+                f.read(8 if self.code_str[16:17] == 'A' else 4), 
+                byteorder='little', 
+                signed=False
+            )
             self.par['RealSamplesQuantity'] = self.real_samp
             _tt = f.read(10 if self.code_str[16:17] == 'A' else 8)
             self.adc_rate = struct.unpack('<d', f.read(8 if self.code_str[16:17] == 'A' else 4))[0]
@@ -66,6 +87,9 @@ parser.add_argument(
 
 
 def find_start(mas):
+    """
+    finding start process
+    """
     start_val = sum(mas[0:10])/10
     for ti, tval in enumerate(mas):
         if tval < (start_val)*0.6:
@@ -74,24 +98,29 @@ def find_start(mas):
 
 def main(opt):
     list_file_dir = os.listdir(os.path.join(opt.dir))
-    for file_ in list_file_dir:
-        atr_file = os.path.splitext(file_)
-        if  atr_file[1] == '.par':
-            c = Par_Data(os.path.join(opt.dir, atr_file[0]))
-            pos_start = find_start(c.data)
-            fon = sum(c.data[0:pos_start-100])/(pos_start-100)
-            sum_int = 0
-            item = 0
-            pred_val = 0.00001
-            for val in c.data[pos_start+2000:pos_start+10000]:
-                sum_int += (math.log(fon/val) + math.log(fon/pred_val))/2/(c.adc_rate*1000)
-                pred_val = val
-                item += 1
-            print(file_, '\t', str(sum_int/(item)*1000).ljust(20))
+    with open('outtext.txt', 'w') as f:
+        for file_ in list_file_dir:
+            atr_file = os.path.splitext(file_)
+            if  atr_file[1] == '.par':
+                c = Par_Data(os.path.join(opt.dir, atr_file[0]))
+                pos_start = find_start(c.data)
+                fon = sum(c.data[0:pos_start-100])/(pos_start-100)
+                sum_int = 0
+                item = 0
+                pred_val = 0.00001
+                median = 0
+                for val in c.data[pos_start + config['pos_head']:pos_start + config['pos_tail']]:
+                    sum_int += (math.log(fon/val) + math.log(fon/pred_val))/2/(c.adc_rate*1000)
+                    median += math.log(fon/val)
+                    pred_val = val
+                    item += 1
+                str_out = f'{atr_file[0]} \t int = {sum_int} \t median = {median/item}'
+                print(str_out)
+                f.write(str_out + '\n')
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    
     logging.basicConfig(
         level=logging.INFO, 
         format='[%(asctime)s] %(levelname).1s %(message)s', 
